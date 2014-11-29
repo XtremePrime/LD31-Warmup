9a69358b4af3fd78516e8bf7ce1ccdcddbec5cd9
Xtreme Prime <XtremePrimeDev@gmail.com>
2014-11-29 16:59:29 +0200

    Background

--- LD31-Warmup/src/states/introstate.cpp
+++ LD31-Warmup/src/states/introstate.cpp
@@ -2,6 +2,7 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 #include <iostream>
+#include <cmath>
 
 IntroState* IntroState::_instance;
 
@@ -14,16 +15,22 @@ void IntroState::init()
 {
 	//- Texture loading
 	_tx_mgr.load_texture("wall", "res/img/wall.png");
+	_tx_mgr.load_texture("background", "res/img/bg.png");	
 
 	//- Sprite "loading"
 	_sprites["wall"].setTexture(_tx_mgr.get_ref("wall"));
+	_sprites["background"].setTexture(_tx_mgr.get_ref("background"));	
 
 	_font.loadFromFile("res/font/PressStart2P.ttf");
 	_title.setString("Untitled Stuff");
 	_title.setFont(_font);
 	_title.setCharacterSize(30);
-	_title.setColor(sf::Color::White);
+	_title.setColor(sf::Color::Black);
 	_title.setPosition(100, 100);
+
+	_sprites["background"].setScale((float)(640)/250, (float)(480)/190);
+	std::cout << _sprites["background"].getScale().x << "\n";
+
 }
 
 void IntroState::cleanup()
@@ -43,13 +50,13 @@ void IntroState::update(GameEngine* game, sf::Time deltaTime)
 
 void IntroState::render(GameEngine* game)
 {
-	game->get_window()->draw(_title);
-	
 	//- Render sprites
 	#define it_type std::map<std::string, sf::Sprite>::iterator
 	for(it_type it = _sprites.begin(); it != _sprites.end(); ++it)
 		game->get_window()->draw(it->second);
 	#undef it_type
+
+	game->get_window()->draw(_title);
 }
 
 void IntroState::pause()
